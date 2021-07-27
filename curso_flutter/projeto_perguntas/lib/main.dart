ff33fa25a00fd0699888476db8dbf4ec0cf13df7
Germano Lira <dolan.3@hotmail.com>
2021-07-26 21:42:23 -0300

    projeto perguntas

--- curso_flutter/projeto_perguntas/lib/main.dart
+++ curso_flutter/projeto_perguntas/lib/main.dart
@@ -1,20 +1,69 @@
 import 'package:flutter/material.dart';
+import './questao.dart';
+import './resposta.dart';
+
+// Questao(perguntas[_perguntaSelecionada]['texto'].toString()),
+/*
+for (String textoResp in perguntas[_perguntaSelecionada].cast()['respostas']) {
+      respostas.add(Resposta(textoResp, _responder));
+    }
+*/
 
 void main() {
   runApp(PerguntaApp());
 }
 
-class PerguntaApp extends StatelessWidget {
+class PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
+
+  void _responder() {
+    setState(() {
+      _perguntaSelecionada++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final List<Map<String, Object>> perguntas = [
+      {
+        'texto': 'Qual é a sua cor favorita?',
+        'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
+      },
+      {
+        'texto': 'Qual é o seu animal favorito?',
+        'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
+      },
+      {
+        'texto': 'Qual é o seu instrutor favorito?',
+        'respostas': ['Maria', 'João', 'Leo', 'Pedro'],
+      }
+    ];
+
+    for (String textoResp
+        in perguntas[_perguntaSelecionada].cast()['respostas']) {
+      respostas.add(Resposta(textoResp, _responder));
+    }
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Pergunta'),
+          title: Text('Perguntas'),
         ),
-        body: Center(
-          child: Text('Pergunta')),
+        body: Column(
+          children: [
+            Questao(perguntas[_perguntaSelecionada]['texto'].toString()),
+            Resposta('Resposta 1', _responder),
+            Resposta('Resposta 2', _responder),
+            Resposta('Resposta 3', _responder),
+          ],
         ),
-      );
+      ),
+    );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  PerguntaAppState createState() {
+    return new PerguntaAppState();
   }
 }
